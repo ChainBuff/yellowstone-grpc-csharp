909155a3671bee69ef1703630cd61a9cea9ef141
icaca <icaca@live.com>
2024-12-30 20:11:59 +0800

    add proto files

---
+++ 00-sub/Program.cs
@@ -0,0 +1,11 @@
+﻿using Grpc.Net.Client;
+using System.Threading.Tasks;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+    }
+}
