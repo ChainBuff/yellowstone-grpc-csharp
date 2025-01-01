c3bbc235226f0c8c00c0ccf66845285e1b0c2906
icaca <icaca@live.com>
2025-01-01 15:43:40 +0800

    订阅slots

--- 00-sub/Program.cs
+++ 00-sub/Program.cs
@@ -1,11 +1,36 @@
 ﻿using Grpc.Net.Client;
-using System.Threading.Tasks;
+using GrpcGeyser;
 
-internal class Program
+using var channel = GrpcChannel.ForAddress("https://solana-yellowstone-grpc.publicnode.com:443");
+var client = new Geyser.GeyserClient(channel);
+var request = new SubscribeRequest
 {
-    private static void Main(string[] args)
+    Commitment = CommitmentLevel.Processed
+};
+
+request.Slots.Add("slot", new SubscribeRequestFilterSlots { FilterByCommitment = true });
+
+using var stream = client.Subscribe();
+await stream.RequestStream.WriteAsync(request);
+
+// move to the next message within the reply stream
+var cancellationToken = new CancellationToken();
+
+
+while (await stream.ResponseStream.MoveNext(cancellationToken))
+{
+    // get the current message within reply stream
+    var message = stream.ResponseStream.Current;
+
+    // print the current message
+
+    if (message.Ping != null)
     {
-        Console.WriteLine("Hello, World!");
 
     }
+
+    Console.WriteLine(message.ToString());
 }
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
