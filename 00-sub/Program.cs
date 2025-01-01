31d9e78b5a4d3f1de90b7503acacefe357dc282b
icaca <icaca@live.com>
2025-01-01 22:13:55 +0800

    Update Program.cs

--- 00-sub/Program.cs
+++ 00-sub/Program.cs
@@ -7,20 +7,35 @@ var request = new SubscribeRequest
 {
     Commitment = CommitmentLevel.Processed
 };
-
 request.Slots.Add("slot", new SubscribeRequestFilterSlots { FilterByCommitment = true });
+
+
+var pingRequest = new SubscribeRequest
+{
+    Ping = new SubscribeRequestPing
+    {
+        Id = 1
+    }
+};
 
 using var stream = client.Subscribe();
 
 await stream.RequestStream.WriteAsync(request);
 var cancellationToken = new CancellationToken();
-
-
-while (await stream.ResponseStream.MoveNext(cancellationToken))
+Timer timer = new(async (state) =>
 {
-    var message = stream.ResponseStream.Current;
-    Console.WriteLine(message.ToString());
-}
+    await stream.RequestStream.WriteAsync(pingRequest);
+    ;
+}, null, 1000, 5000);
 
-Console.WriteLine("Press any key to exit...");
-Console.ReadKey();
+Task responseTask = Task.Run(async () =>
+{
+    while (await stream.ResponseStream.MoveNext(cancellationToken))
+    {
+        var message = stream.ResponseStream.Current;
+        Console.WriteLine(message.ToString());
+    }
+});
+
+// 等待响应流处理完成或用户按键退出
+await Task.WhenAny(responseTask, Task.Run(() => Console.ReadKey()));
